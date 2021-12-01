12a4615e7f9e782f6d2a0432b06c613b35544989
Md Hefzul Bari <ratul14april@gmail.com>
2021-11-30 23:04:07 -0600

    Created a timer with alarm

--- timer.js
+++ timer.js
@@ -4,13 +4,13 @@ const timer = function(number) {
   let numArr = [];
   let sortedArr = [];
   let delay = 0;
-  for (let n = 0; n < number.length; n++){
-    if(number[n] > 0 && !isNaN(number[n])){
+  for (let n = 0; n < number.length; n++) {
+    if (number[n] > 0 && !isNaN(number[n])) {
       numArr.push(number[n])
       sortedArr = numArr.sort((a, b) => (a - b));
     };
   };
-  for (let i = 0; i < sortedArr.length; i++){
+  for (let i = 0; i < sortedArr.length; i++) {
     setTimeout(() => {
       process.stdout.write('\x07');
       console.log(sortedArr[i] + " seconds");
